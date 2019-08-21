5ea7c4481fd29d002f120dcee079101855ce73f5
Fang-Xust <48410354+Fang-Xust@users.noreply.github.com>
2019-08-21 23:39:14 +0800

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -137,4 +137,26 @@ public class LinkedList {
         }
         return node;
     }
+    //合并两个有序链表
+    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode cur = dummy;
+        while(l1 != null && l2 != null){
+            if(l1.val <= l2.val){
+                cur.next = l1;
+                l1 = l1.next;
+            }else{
+                cur.next = l2;
+                l2 = l2.next;
+            }
+            cur = cur.next;
+        }
+        if(l1 == null){
+            cur.next = l2;
+        }else if(l2 == null){
+            cur.next = l1;
+        }
+        //上面的if语句可以写成cur.next = l1 == null ? l2 : l1;
+        return dummy.next;
+    }
 }
