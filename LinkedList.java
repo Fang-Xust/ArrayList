22bb7c1b68d8aa2b68a7c0cd1792059339b168c8
Fang-Xust <48410354+Fang-Xust@users.noreply.github.com>
2019-08-23 23:29:57 +0800

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -195,4 +195,31 @@ public class LinkedList {
             return small;
         }
     }
+    //链表的回文结构
+    public static boolean chkPalindrome(ListNode head){
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        //反转后半部分链表，比如 1 -> 2 -> 1 -> null   反转后为  1 -> null <- 2 <- 1
+        ListNode prev = reverseList(slow);
+        //定义两个结点记录头，和尾，开始从两端比较
+        ListNode pHead = head;
+        ListNode tail = prev;
+        boolean flag = true;
+        while(tail != null){
+            if(pHead.val != tail.val){
+                flag = false;
+                break;
+            }
+            pHead = pHead.next;
+            tail = tail.next;
+        }
+        //比较完以后还原原来的链表结构，因为比较的过程改变了原有链表
+        reverseList(prev);
+        //返回结果
+        return flag;
+    }
 }
