050168e72ca780194eda0008110a495bf5b0fbab
Fang-Xust <48410354+Fang-Xust@users.noreply.github.com>
2019-08-24 22:23:39 +0800

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -222,4 +222,44 @@ public class LinkedList {
         //返回结果
         return flag;
     }
+    //求链表长度
+    public static int getLength(ListNode head){
+        int len = 0;
+        for(ListNode cur = head; cur != null; cur = cur.next){
+            len++;
+        }
+        return len;
+    }
+    //相交链表判断
+    public static ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        //计算两个链表的长度，并求差值
+        ListNode l1 = headA;
+        ListNode l2 = headB;
+        int lenA = getLength(l1);
+        int lenB = getLength(l2);
+        ListNode longer = l1;
+        ListNode shorter = l2;
+        int diff = lenA - lenB;
+        //调整两个链表的长度及差值
+        if(lenA < lenB){
+            longer = l2;
+            shorter = l1;
+            diff = lenB - lenA;
+        }
+        //判空
+        if(longer == null || shorter == null){
+            return null;
+        }
+        //让长的先走两链表之差步
+        for(int i = 0; i < diff ; i++){
+            longer = longer.next;
+        }
+        //再同时向后遍历，不相同就一直往后遍历，如果有结点相同就跳出循环
+        while(longer != shorter){
+            longer = longer.next;
+            shorter = shorter.next;
+        }
+        //返回此结点
+        return longer;
+    }
 }
