1d33ed11163ff946eed7d9b1601bbda4e6890145
Fang-Xust <48410354+Fang-Xust@users.noreply.github.com>
2019-08-26 19:22:43 +0800

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -279,4 +279,33 @@ public class LinkedList {
         }
         return false;
     }
+    //环形链表求入环点
+    public ListNode detectCycle(ListNode head) {
+        //思路：快慢指针法
+        ListNode slow = head;
+        ListNode fast = head;
+        while(slow != null && fast != null){
+            if(fast.next == null){
+                return null;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+            //如果快慢指针相遇，就跳出循环
+            if(slow == fast){
+                break;
+            }
+            //如果快、慢指针中，任意一个为空，就说明链表无环，返回null
+            if(slow == null || fast == null){
+                return null;
+            }
+        }
+        //找到相遇点后，定义一个指向链表开头的指针pHead，让pHead和slow慢指针同时走，
+        //则它们肯定会在入环点相遇，返回入环点
+        ListNode pHead = head;
+        while(pHead != slow && slow != null && pHead != null){
+            pHead = pHead.next;
+            slow = slow.next;
+        }
+        return pHead;
+    }
 }
